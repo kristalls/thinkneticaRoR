d1eed1cb6f09ea45983a6053f43937e95f9318f0
Kirill Churakov <35890160+kristalls@users.noreply.github.com>
2018-07-19 11:00:44 +0300

    Add files via upload

--- lesson1/weight_calc.rb
+++ lesson1/weight_calc.rb
@@ -2,9 +2,10 @@ puts "Укажите ваше имя:"
 name = gets.chomp
 puts "Здравствуйте, #{name}!"
 puts "Укажите ваш рост:"
-height = gets.chomp
-ideal_weight = height.to_i - 110
-if ideal_weight <= 0 then puts "Ваш вес уже идеален"
+height = gets.to_i
+ideal_weight = height - 110
+if ideal_weight <= 0 
+  puts "Ваш вес уже идеален"
 else
-puts "Ваш идеальный вес - #{ideal_weight} кг."
+  puts "Ваш идеальный вес - #{ideal_weight} кг."
 end
