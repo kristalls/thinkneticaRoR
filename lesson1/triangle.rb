c40afe10c00dd9294e8fb74bf342fbf72a318eb7
Kirill Churakov <35890160+kristalls@users.noreply.github.com>
2018-07-22 06:03:50 +0300

    Add files via upload

--- lesson1/triangle.rb
+++ lesson1/triangle.rb
@@ -1,25 +1,24 @@
 puts "Enter the sides of triagle"
 
 print "a = "
-a = gets.chomp.to_i
+a = gets.chomp.to_f
 
 print "b = "
-b = gets.chomp.to_i
+b = gets.chomp.to_f
 
 print "c = "
-c = gets.chomp.to_i
+c = gets.chomp.to_f
 
 sides = [a, b, c].sort!
-rectangular? = sides[2]**2 == sides[1]**2 + sides[0]**2
-isosceles? = sides[1] == sides[2] && sides[1] == sides[0]
-equilateral? = sides[1] == sides[0]
-if isosceles
+
+if sides[1] == sides[2] && sides[1] == sides[0]
   puts "Треугольник равносторонний"
   exit
-elsif rectangular
+elsif sides[2]**2 == sides[1]**2 + sides[0]**2
   puts "Треугольник прямоугольный"
-elsif equilateral
-  puts "Треугольник равнобедренный"
+    if sides[1] == sides[0]
+      puts "Треугольник равнобедренный"
+    end
 end
 
 
